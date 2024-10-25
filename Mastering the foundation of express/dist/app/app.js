1f4edb04f44dc40b9836e86b76b39b162b25af23
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2024-10-25 20:36:55 +0600

    Explore routing

--- Mastering the foundation of express/dist/app/app.js
+++ Mastering the foundation of express/dist/app/app.js
@@ -10,11 +10,48 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 // convert text data
 app.use(express_1.default.text());
+//Normal Get
 app.get("/", (req, res) => {
     res.send("Server Running at 5500");
 });
+//Middlware used for check anything before hit the actual API
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+// Get with Id and sub Id
+app.get("/:userId/:subId", logger, (req, res) => {
+    console.log(req.params);
+    //{ userId: '45', subId: '32' }
+    res.send(req.params);
+});
+//Normal Post
 app.post("/", (req, res) => {
     console.log(req.body);
     res.send("Got the data");
+});
+//create router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+// use router
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+// create api using user router
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    res.json({
+        success: true,
+        message: "user created",
+        data: user,
+    });
+});
+// create api using user router
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    res.json({
+        success: true,
+        message: "course created",
+        data: course,
+    });
 });
 exports.default = app;
--- Mastering the foundation of express/src/app/app.ts
+++ Mastering the foundation of express/src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
@@ -8,13 +8,55 @@ app.use(express.json());
 // convert text data
 app.use(express.text());
 
+//Normal Get
 app.get("/", (req: Request, res: Response) => {
   res.send("Server Running at 5500");
 });
 
+//Middlware used for check anything before hit the actual API
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
+
+// Get with Id and sub Id
+app.get("/:userId/:subId", logger, (req: Request, res: Response) => {
+  console.log(req.params);
+  //{ userId: '45', subId: '32' }
+  res.send(req.params);
+});
+
+//Normal Post
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.send("Got the data");
+});
+
+//create router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+// use router
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+// create api using user router
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  res.json({
+    success: true,
+    message: "user created",
+    data: user,
+  });
+});
+// create api using user router
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  res.json({
+    success: true,
+    message: "course created",
+    data: course,
+  });
 });
 
 export default app;
